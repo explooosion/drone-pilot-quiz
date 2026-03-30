d775c1cafef32b77fb4c90fb993662683f4f863c
Robby Wu <ta7382@gmail.com>
2026-03-31 00:23:05 +0800

    feat: switch HashRouter to BrowserRouter, add 404.html SPA redirect

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { ConsentProvider } from './contexts/ConsentContext';
 import { App } from './App';
@@ -8,12 +8,12 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <ThemeProvider>
         <ConsentProvider>
           <App />
         </ConsentProvider>
       </ThemeProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 );
